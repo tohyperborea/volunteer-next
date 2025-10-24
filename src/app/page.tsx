3408da1a22a6c01cc67e135a1007cac94b792ff8
Michael Townsend <michael@doublespeakgames.com>
2025-10-24 18:33:35 -0400

    working on integrating pretix oauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,31 @@
  */
 
 import { Flex, Text } from '@radix-ui/themes';
-import db from '../db';
+import db from '@/db';
+import { auth } from '@/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default async function MyApp() {
+  const authData = await auth.api.signInWithOAuth2({
+    body: {
+      providerId: 'local-pretix',
+      callbackURL: '/',
+      scopes: ['openid']
+    }
+  });
+  console.log('Auth Data:', authData);
+
+  if (authData.redirect) {
+    redirect(authData.url);
+    return;
+  }
+
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+  console.log('Session Data:', session);
+
   const dbValue = (await db.query('SELECT 1 as value')).rows[0].value; // Ensure DB is initialized
   return (
     <Flex direction="column" gap="2">
--- src/auth.ts
+++ src/auth.ts
@@ -5,5 +5,25 @@
  */
 
 import { betterAuth } from 'better-auth';
+import { genericOAuth } from 'better-auth/plugins';
+import db from '@/db';
 
-export const auth = betterAuth({});
+if (!process.env.OAUTH_CLIENT_ID) {
+  throw new Error('OAUTH_CLIENT_ID is not set in environment variables');
+}
+
+export const auth = betterAuth({
+  plugins: [
+    genericOAuth({
+      config: [
+        {
+          providerId: 'local-pretix',
+          clientId: process.env.OAUTH_CLIENT_ID,
+          clientSecret: process.env.OAUTH_CLIENT_SECRET,
+          discoveryUrl: 'http://localhost:8000/testorg/.well-known/openid-configuration'
+        }
+      ]
+    })
+  ],
+  database: db
+});
