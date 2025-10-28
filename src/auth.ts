4a35dd1e6f918d8d1cab585b83f8aa5b50f2dda8
Michael Townsend <michael@doublespeakgames.com>
2025-10-28 11:51:31 -0400

    OAuth from Pretix (#4)
    * Add OAuth plugin to Better Auth
    * Add simple test UI to homepage
    * Update README

--- src/auth.ts
+++ src/auth.ts
@@ -5,5 +5,44 @@
  */
 
 import { betterAuth } from 'better-auth';
+import { nextCookies } from 'better-auth/next-js';
+import { genericOAuth } from 'better-auth/plugins';
+import db from '@/db';
 
-export const auth = betterAuth({});
+if (!process.env.OAUTH_CLIENT_ID) {
+  throw new Error('OAUTH_CLIENT_ID is not set in environment variables');
+}
+
+if (!process.env.OAUTH_PROVIDER_ID) {
+  throw new Error('OAUTH_PROVIDER_ID is not set in environment variables');
+}
+
+if (!process.env.OAUTH_CLIENT_SECRET) {
+  throw new Error('OAUTH_CLIENT_SECRET is not set in environment variables');
+}
+
+if (!process.env.OAUTH_DISCOVERY_URL) {
+  throw new Error('OAUTH_DISCOVERY_URL is not set in environment variables');
+}
+export const auth = betterAuth({
+  plugins: [
+    genericOAuth({
+      config: [
+        {
+          providerId: process.env.OAUTH_PROVIDER_ID,
+          clientId: process.env.OAUTH_CLIENT_ID,
+          clientSecret: process.env.OAUTH_CLIENT_SECRET,
+          discoveryUrl: process.env.OAUTH_DISCOVERY_URL
+        }
+      ]
+    }),
+    nextCookies()
+  ],
+  database: db,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: 'none',
+      secure: true
+    }
+  }
+});
