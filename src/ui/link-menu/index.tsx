ba6f7565b93f43144632695e62960adfc748d282
Michael Townsend <michael@doublespeakgames.com>
2026-03-03 14:39:49 -0500

    add tests for LinkMenu

--- src/ui/link-menu/index.tsx
+++ src/ui/link-menu/index.tsx
@@ -6,20 +6,21 @@
 
 'use client';
 
-import { Box, Button, Flex } from '@radix-ui/themes';
+import { Button, Flex } from '@radix-ui/themes';
 import { useState } from 'react';
 import styles from './styles.module.css';
 import { ChevronDownIcon } from '@radix-ui/react-icons';
 
 export interface Props {
-  children?: React.ReactNode[];
+  children: React.ReactElement | React.ReactElement[];
 }
 
 export default function LinkMenu({ children }: Props) {
+  const childList = Array.isArray(children) ? children : [children];
   return (
     <Flex direction={'column'} gap="2" p="0" asChild>
       <ul className={styles.list}>
-        {children?.map((item, idx) => (
+        {childList?.map((item, idx) => (
           <li key={idx}>{item}</li>
         ))}
       </ul>
@@ -29,13 +30,14 @@ export default function LinkMenu({ children }: Props) {
 
 export interface SubLinkMenuProps {
   title: string;
-  children?: React.ReactNode[];
+  children: React.ReactElement | React.ReactNode[];
 }
 
 export function SubLinkMenu({ title, children }: SubLinkMenuProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const childList = Array.isArray(children) ? children : [children];
   return (
-    <Flex className={isOpen ? styles.open : ''} direction={'column'}>
+    <Flex direction={'column'}>
       <Flex asChild justify="between">
         <Button color="gray" variant="soft" size="3" onClick={() => setIsOpen((open) => !open)}>
           {title}
@@ -43,8 +45,8 @@ export function SubLinkMenu({ title, children }: SubLinkMenuProps) {
         </Button>
       </Flex>
       <Flex direction={'column'} gap="2" p="4" asChild>
-        <ul className={`${styles.sublist} ${styles.list}`}>
-          {children?.map((item, idx) => (
+        <ul className={styles.list} style={!isOpen ? { display: 'none' } : undefined}>
+          {childList?.map((item, idx) => (
             <li key={idx}>{item}</li>
           ))}
         </ul>
